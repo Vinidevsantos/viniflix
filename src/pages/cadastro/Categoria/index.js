b6d13c03b39c51a0db18414ced6d596f107b89b4
Vinicius Santos <vinisantos.rv@gmail.com>
2020-07-30 16:08:44 -0300

    correcao

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -27,14 +27,15 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-      const URL_TOP = "http://localhost:8080/categorias";
-      fetch(URL_TOP)
-       .then( async (respostaDoServidor) => {
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
         setCategorias([
           ...resposta,
         ]);
       });
+  }, []);
 
   return (
     <PageDefault>
