2229ca966227d026cb2e07c215b97052c91b8c25
Vinicius Santos <vinisantos.rv@gmail.com>
2020-07-31 15:34:27 -0300

    http para https
    http para https

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'http://viniflix-alura.herokuapp.com';
+  ? 'https://localhost:8080'
+  : 'https://viniflix-alura.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
